43b78780c8642e9ef8b8ab4e71ad29fcb6b527c6
sobhan <sobhandanger81@gmail.com>
2024-06-10 00:06:15 +0430

    project finished

--- admin-page/main.js
+++ admin-page/main.js
@@ -8,11 +8,18 @@ const logout = document.querySelector(".logout");
 const userType = document.querySelector(".usertype");
 const usernameSpan = document.querySelector(".username");
 
+const newLessonName = document.querySelector("#course-name");
+const newLessonTeacher = document.querySelector("#instructor-name");
+const newLessonDay1 = document.querySelector("#class-day-1");
+const newLessonDay2 = document.querySelector("#class-day-2");
+const newLessonTimeFrom = document.querySelector("#start-time");
+const newLessonTimeTo = document.querySelector("#end-time");
+const addNewLessonBtn = document.querySelector(".add-new-course");
+
 let userInfo =
   localStorage.getItem("userInfo") &&
   JSON.parse(localStorage.getItem("userInfo"));
 let courses = [];
-
 
 if ([null, undefined, ""].includes(userInfo)) {
   window.location.href = "login/index.html";
@@ -41,7 +48,7 @@ const createTable = () => {
     addBtn.classList.add("btn");
     addBtn.classList.add("remove");
     addBtn.addEventListener("click", () => {
-      addCourse(course);
+      removeRow(course);
     });
     actionTd.appendChild(addBtn);
     tr.appendChild(lessonNameTd);
@@ -55,16 +62,6 @@ const createTable = () => {
 };
 
 (() => {
-  fetch(`${BASE_URL}users`, {})
-    .then((res) => res.json())
-    .then((res) => {
-      let userPickedLessons = res?.find(
-        (el) => el.username === userInfo?.username
-      )?.pickedLessons;
-      selectedCourses = userPickedLessons;
-    })
-    .catch((err) => console.error(err));
-
   fetch(`${BASE_URL}lessons`, {})
     .then((res) => res.json())
     .then((res) => {
@@ -74,42 +71,17 @@ const createTable = () => {
     .catch((err) => console.error(err));
 })();
 
-function convertTimeToNumber(time) {
-  const splitTime = time.split(":");
-  return parseInt(splitTime[0] * 3600 + splitTime[1] * 60);
-}
-
-function addCourse(selectedCourse) {
-  if (
-    courses.find(
-      (course) => course.lessonName === selectedCourse.lessonName
-    )
-  ) {
-    notify.textContent = "شما این درس را قبلا انتخاب کرده اید";
-    notify.classList.add("show-notification");
-    notify.classList.add("error");
-    setTimeout(() => {
-      notify.innerHTML = "";
-      notify.classList.remove("show-notification");
-    }, 3000);
-    return;
-  } else if (
-    courses.find(
-      (course) =>
-        (course.days.includes(selectedCourse.days[0]) ||
-          course.days.includes(selectedCourse.days[1])) &&
-        ((convertTimeToNumber(selectedCourse.timeFrom) <=
-          convertTimeToNumber(course.timeTo) &&
-          convertTimeToNumber(selectedCourse.timeFrom) >=
-            convertTimeToNumber(course.timeFrom)) ||
-          (convertTimeToNumber(selectedCourse.timeTo) <=
-            convertTimeToNumber(course.timeTo) &&
-            convertTimeToNumber(selectedCourse.timeTo) >=
-              convertTimeToNumber(course.timeFrom)))
-    )
-  ) {
-    notify.textContent =
-      " درس انتخاب شده با سایر درس هایی که انتخاب کرده اید تداخل زمانی دارد";
+function addCourse() {
+  let newCourse = {
+    isPresented: true,
+    lessonName: newLessonName.value.trim(),
+    professorName: newLessonTeacher.value.trim(),
+    days: [newLessonDay1.value.trim(), newLessonDay2.value.trim()],
+    timeFrom: newLessonTimeFrom.value.trim(),
+    timeTo: newLessonTimeTo.value.trim(),
+  };
+  if (Object.values(newCourse).flat().includes("")) {
+    notify.textContent = "پرکردن تمامی فیلدها الزامی است.";
     notify.classList.add("show-notification");
     notify.classList.add("error");
     setTimeout(() => {
@@ -118,27 +90,67 @@ function addCourse(selectedCourse) {
       notify.classList.remove("error");
     }, 3000);
     return;
+  } else {
+    fetch(BASE_URL + "lessons", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newCourse),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        courses = [...courses, res];
+        createTable();
+        newLessonName.value = "";
+        newLessonTeacher.value = "";
+        newLessonDay1.value = "";
+        newLessonDay2.value = "";
+        newLessonTimeFrom.value = "";
+        newLessonTimeTo.value = "";
+        notify.textContent = "درس انتخاب شده با موفقیت اضافه شد.";
+        notify.classList.add("show-notification");
+        notify.classList.add("success");
+        setTimeout(() => {
+          notify.innerHTML = "";
+          notify.classList.remove("show-notification");
+          notify.classList.remove("success");
+        }, 3000);
+      });
   }
-  courses.push(selectedCourse);
-  addToSelectedCourse();
-  notify.textContent = "درس انتخاب شده با موفقیت اضافه شد.";
-  notify.classList.add("show-notification");
-  notify.classList.add("success");
-  setTimeout(() => {
-    notify.innerHTML = "";
-    notify.classList.remove("show-notification");
-    notify.classList.remove("success");
-  }, 3000);
 }
 
-function removeRow(course) {
-  courses = courses.filter(
-    (el) => el.lessonName !== course.lessonName
-  );
-  addToSelectedCourse();
+function removeRow(selectedCourse) {
+  fetch(BASE_URL + "lessons/" + selectedCourse.lessonId, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({}),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      courses = courses.filter(
+        (course) => course.lessonId !== selectedCourse.lessonId
+      );
+      createTable();
+
+      notify.textContent = "درس انتخاب شده با موفقیت حذف شد.";
+      notify.classList.add("show-notification");
+      notify.classList.add("success");
+      setTimeout(() => {
+        notify.innerHTML = "";
+        notify.classList.remove("show-notification");
+        notify.classList.remove("success");
+      }, 3000);
+    })
+    .catch((err) => console.error(err));
 }
 
 logout.addEventListener("click", () => {
   localStorage.clear();
   window.location.href = "login/index.html";
+});
+
+
+addNewLessonBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addCourse();
 });
